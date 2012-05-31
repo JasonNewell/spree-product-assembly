f14b08c181da480de10b549e13a7c8d0484690f4
Jason Newell <jason.newell@touchstudios.net>
2012-05-30 19:09:30 -0500

    Got gem running, fixed styling bug. Working on fixing circular dependencies, and improved behavior for part search (either leaving the list in place when an item is selected, or clearing the search box)

--- app/models/spree/assemblies_part.rb
+++ app/models/spree/assemblies_part.rb
@@ -3,6 +3,22 @@ class Spree::AssembliesPart < ActiveRecord::Base
   belongs_to :assembly, :class_name => "Spree::Product", :foreign_key => "assembly_id"
   belongs_to :part, :class_name => "Spree::Variant", :foreign_key => "part_id"
 
+  #validate :no_circular_dependencies - Needs to be handled in product decorator
+  #
+  #def no_circular_dependencies
+  #  errors.add_to_base('This would create a circular dependency.') if has_circular_dependencies(self)
+  #end
+  #
+  #def has_circular_dependencies?(assembly)
+  #  if assembly == part
+  #    return false
+  #  elsif part.assembly?
+  #    return part.has_circular_dependencies?(assembly)
+  #  else
+  #    return true
+  #  end
+  #end
+
   def self.get(assembly_id, part_id)
     Spree::AssembliesPart.find_by_assembly_id_and_part_id(assembly_id, part_id)
   end
@@ -15,5 +31,4 @@ class Spree::AssembliesPart < ActiveRecord::Base
   def destroy
     Spree::AssembliesPart.delete_all(["assembly_id = ? AND part_id = ?", assembly_id, part_id])
   end
-
 end
